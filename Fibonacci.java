c5fe9f64d92a6fa94be0aecd424e0aac9111c445
Dhananjay Pawar <idhananjaypawar@gmail.com>
2025-08-31 19:05:26 +0530

    Triangle Pattern

--- Fibonacci.java
+++ Fibonacci.java
@@ -11,7 +11,7 @@ class Fibonacci{
 
     static void squarePattern(int n){
         for(int i=1;i<=n;i++){
-            for (int j = 1; j <=n; j++) {
+            for (int j = 1; j <=i; j++) {
                 System.out.print("* ");  // * * * * *
             }
             System.out.println(); // EmptyLinePrinting
